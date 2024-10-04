50a2d55f8c66383bae91142384d115caabff4990
solxire88 <136970695+solxire88@users.noreply.github.com>
2024-10-04 17:17:01 +0100

    added loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import logo from "./assets/Pelorus.png";
 import AboutUs from "./Components/aboutUs.jsx";
@@ -6,37 +6,58 @@ import Benefits from "./Components/benefits.jsx";
 import Projects from "./Components/projects.jsx";
 import ContactUs from "./Components/contactUs.jsx";
 import { motion, useInView, useAnimation } from "framer-motion";
+import BarLoader from "react-spinners/BarLoader";
 import "./App.css";
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
+
   return (
-    <div className="bigContainer">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Pelorus</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-        <link rel="icon" type="image/svg+xml" href="facicon.svg" />
-        <meta name="description" content="Helmet application" />
-      </Helmet>
-      <Title></Title>
-      <AboutUs></AboutUs>
-      {/* <Benefits></Benefits> */}
-      <Projects></Projects>
-      <ContactUs></ContactUs>
-    </div>
+    <>
+      {loading ? (
+        <div className="loading">
+          <img src="src/facicon.svg" height="50"></img>
+          <BarLoader color="#FFFFFF" width="75"></BarLoader>
+        </div>
+      ) : (
+        <div className="bigContainer">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Pelorus</title>
+            <link rel="canonical" href="http://mysite.com/example" />
+            <link rel="icon" type="image/svg+xml" href="facicon.svg" />
+            <meta name="description" content="Helmet application" />
+          </Helmet>
+          <Title></Title>
+          <AboutUs></AboutUs>
+          {/* <Benefits></Benefits> */}
+          <Projects></Projects>
+          <ContactUs></ContactUs>
+        </div>
+      )}
+    </>
   );
 }
 
 function Title() {
   return (
-    <motion.div className="titlePelorus"
-    variants={{
-      hidden: {opacity: 0, y: -50},
-      visible: {opacity: 1, y: 0}
-    }}
-    initial="hidden"
-    animate="visible"
-    transition={{ duration: 1.5, delay:0.25}}>
+    <motion.div
+      className="titlePelorus"
+      variants={{
+        hidden: { opacity: 0, y: -50 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 1.5, delay: 0.25 }}
+    >
       <img
         className="pelorusImage"
         draggable="false"
